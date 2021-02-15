6cbb7ab30a8077237399554b20c44bd87be44624
Frédéric Boulanger <frederic.boulanger@universite-paris-saclay.fr>
2021-02-15 11:16:07 +0100

    Fixed issue in run-docker-framac.sh

--- run-docker-framac.sh
+++ run-docker-framac.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-platform=`uname -m`
-
 case `uname` in
 	Darwin)
 		open -a XQuartz
@@ -16,4 +14,4 @@ docker run --rm --tty --interactive \
 			 --volume "$(PWD):/workspace:rw" \
 			 --name framac2021 \
 			 --env="DISPLAY=host.docker.internal:0" \
-			 fredblgr/framac-${platform}:2021
+			 fredblgr/framac:2021
